1292f951e51d5b5a4e21fdf6e17574e1e414d877
GGLionCross <lionelquiambao@gmail.com>
2019-08-07 01:36:59 -0700

    Built webpack and added src/index.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,9 @@
 const HtmlWebPackPlugin = require("html-webpack-plugin");
 const path = require("path");
 module.exports = {
+  output: {
+    path: path.join(__dirname, 'docs')
+  },
   module: {
     rules: [
       {
